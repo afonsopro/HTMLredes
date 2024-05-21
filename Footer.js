cead9487770e9e1165389eb8402b3b1a61949540
PraxiPrime <153953407+PraxiPrime@users.noreply.github.com>
2024-05-21 14:27:50 +0100

    Create Footer.js

---
+++ Footer.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const anchorLinks = document.querySelectorAll('a[href^="#"]');
+
+    for (let link of anchorLinks) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        });
+    }
+});
